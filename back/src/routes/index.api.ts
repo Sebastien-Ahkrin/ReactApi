9bbee9c0dee782abf778fba0b1b257e8bf3c33ee
Sebastien Ahkrin <sebastien48criquet@gmail.com>
2019-10-22 13:36:47 +0200

    Add validator route

--- back/src/routes/index.api.ts
+++ back/src/routes/index.api.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from "express"
 import { router } from './../server'
 
-router.get('/', async (request: Request, response: Response) => {
-  response.status(200).json({ text: 'Hello, World!' })
+const path: string = '/security'
+
+const emailRegex: string = /^[^\W][a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\@[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\.[a-zA-Z]{2,4}$/g.toString()
+const pseudoRegex: string = /^[a-zA-Z]{3,16}$/g.toString()
+const passwordRegex: string = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\-])(?=.{8,})/g.toString()
+
+router.get(`${path}/validate`, async (request: Request, response: Response) => {
+  response.status(200).json({
+    email: emailRegex,
+    pseudo: pseudoRegex,
+    password: passwordRegex
+  })
 })
 
 export default router
